9df214693687ac9becae5543ca4130ac288bf2bf
Rohit <rohit15033@gmail.com>
2025-01-24 16:33:38 +0700

    Initial commit

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home({name}) {
+    return (
+        <div>
+            <div>laravel + React via inertia </div>
+            <div>Hello {name}</div>
+        </div>
+    )
+}
