24614b2a3e8f860db05c8d0dc8df531ecca2289e
KBumstead <115068127+KBumstead@users.noreply.github.com>
2025-02-07 22:48:06 +0700

    model
    model making of appointment might have some changes later on

---
+++ app/Http/Controllers/AppointmentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\appointment;
+use App\Http\Requests\StoreappointmentRequest;
+use App\Http\Requests\UpdateappointmentRequest;
+
+class AppointmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreappointmentRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateappointmentRequest $request, appointment $appointment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(appointment $appointment)
+    {
+        //
+    }
+}
